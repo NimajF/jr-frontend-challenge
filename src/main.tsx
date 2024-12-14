4bbaa7efcf65d8f79a839f649ae17c28252be364
unknown <benjafran2000@gmail.com>
2024-12-13 21:43:29 -0300

    Add Mapbox config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import mapboxgl from "mapbox-gl";
 
-createRoot(document.getElementById('root')!).render(
+mapboxgl.accessToken = import.meta.env.REACT_APP_MAPBOX_TOKEN!;
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

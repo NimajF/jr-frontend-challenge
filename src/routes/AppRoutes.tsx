33b4e08069a272a4d96916b9895df99da09e7d34
unknown <benjafran2000@gmail.com>
2024-12-15 18:17:30 -0300

    Add not-found page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PropertiesList from "../pages/PropertiesList";
 import PropertyDetails from "../pages/PropertyDetails";
 import PropertyForm from "../pages/PropertyForm";
+import EditProperty from "../components/EditPropertyForm";
 import Layout from "../components/Layout";
+import NotFound from "../pages/NotFound";
 
 const AppRoutes = () => {
   return (
@@ -12,7 +14,9 @@ const AppRoutes = () => {
           <Route index element={<PropertiesList />} />
           <Route path="properties/:id" element={<PropertyDetails />} />
           <Route path="properties/new" element={<PropertyForm />} />
-          <Route path="properties/edit/:id" element={<PropertyForm />} />
+          <Route path="properties/edit/:id" element={<EditProperty />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/properties/*" element={<NotFound />} />
         </Route>
       </Routes>
     </Router>
